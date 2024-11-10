bfeeea990efd9b622cff4e3992a148606d944ef2
Abdo <83555913+abdoibrahim257@users.noreply.github.com>
2024-11-10 14:43:20 +0200

    added the loading screen

--- src/app/user-list/user-list.component.ts
+++ src/app/user-list/user-list.component.ts
@@ -1,16 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UserCardComponent } from './user-card/user-card.component';
 import { User } from './user-card/user';
 import { UserService } from '../user.service';
-// import { NgxPaginationModule } from 'ngx-pagination';
 import { PaginationControlsComponent } from './pagination-controls/pagination-controls.component';
+
 @Component({
   selector: 'app-user-list',
   standalone: true,
   imports: [UserCardComponent, PaginationControlsComponent, CommonModule],
   templateUrl: './user-list.component.html',
-  styleUrl: './user-list.component.css'
+  styleUrl: './user-list.component.css',
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 
 export class UserListComponent implements OnInit {
@@ -19,30 +20,40 @@ export class UserListComponent implements OnInit {
   currentPage: number = 1;
   totalItems: number = 0;
   itemsPerPage: number = 0;
-
+  isLoading: boolean = true;
 
   constructor(private userService: UserService) { }
 
   ngOnInit(): void {
-      this.loadUsers();
+    this.loadUsers();
   }
 
-  loadUsers(){
-
-    //loading screen on
-    
+  loadUsers() {
+    this.isLoading = true;
     this.userService.getUsers(this.currentPage).subscribe((data: any) => {
       this.userList = data.data;
       this.itemsPerPage = data.per_page;
       this.totalItems = data.total_pages;
     });
 
-    //loading screen off
+    setTimeout(() => { //because the api is too fast and we can't see the loading spinner
+      this.isLoading = false;
+      this.toggleBodyScrolling();
+    }, 300);
+    this.toggleBodyScrolling();
   }
 
   onPageChange(page: number) {
     this.currentPage = page;
     this.loadUsers();
+  }
+
+  toggleBodyScrolling() {
+    if (this.isLoading) {
+      document.body.style.overflow = 'hidden';
+    } else {
+      document.body.style.overflow = 'auto';
+    }
   }
 
   // this is a dummy users I will fetch with api later
